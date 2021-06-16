7fa6cd29d4e2b1c91e085d80f4db7e98bb5e0649
edward-yose <youngedward472@gmail.com>
2021-06-16 12:14:50 +0700

    update Employee Controller

--- src/controller/EmployeeController.java
+++ src/controller/EmployeeController.java
@@ -1,6 +1,6 @@
 package controller;
 
-import java.util.Vector;
+import java.util.List;
 import model.Employee;
 import model.Transaction;
 import model.TransactionItem;
@@ -8,7 +8,10 @@ import model.TransactionItem;
 
 public class EmployeeController {
 	
-
+	public static List<Employee> getAllEmployee() {
+		return Employee.getAllEmployee();
+	}
+	
 	public EmployeeController() {
 		
 	}
