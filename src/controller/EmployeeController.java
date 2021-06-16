65d06c9202f932f9edd9844e1a7716e2801de482
edward-yose <youngedward472@gmail.com>
2021-06-16 12:11:55 +0700

    delete Controller_HRD, change add EmployeeController

---
+++ src/controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import java.util.Vector;
+import model.Employee;
+import model.Transaction;
+import model.TransactionItem;
+
+
+public class EmployeeController {
+	
+
+	public EmployeeController() {
+		
+	}
+}
