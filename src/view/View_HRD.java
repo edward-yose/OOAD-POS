60c63c432607ceb9668e43aac4fdc14fbdd9cacc
edward-yose <youngedward472@gmail.com>
2021-06-16 14:04:35 +0700

    update View HRD

--- src/view/View_HRD.java
+++ src/view/View_HRD.java
@@ -1,18 +1,16 @@
 package view;
 
-import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JTable;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
-import javax.swing.JTextField;
+import javax.swing.JTable;
 import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableModel;
 
@@ -23,11 +21,16 @@ public class View_HRD extends JFrame{
 	private JButton add_user;
 	private JButton edit_user;
 	private JButton delete_user;
-	private JPanel panel_HRD;
+	private JSpinner spinnerID;
+//	private JSpinner spinnerRoleID;
+//	private JTextField textFieldName;
+//	private JTextField textFileUsername;
+//	private JTextField textFileSalary;
+//	private JPasswordField textFilePassword;
 	
 	private void initLayout() {
 		setSize(800, 600);
-		setTitle("ItemList");
+		setTitle("Employee List");
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -42,6 +45,30 @@ public class View_HRD extends JFrame{
 		
 		scrollPane.setViewportView(table);
 		add(scrollPane);
+		
+		JPanel panel1 = new JPanel();
+		panel1.setLayout(new GridLayout(1, 2, 10, 10));
+		
+		JPanel panel2 = new JPanel();
+		panel2.setLayout(new GridLayout(3, 2));
+		
+		JLabel labelId = new JLabel("Product Id: ");
+		labelId.setVerticalAlignment(SwingConstants.CENTER);
+		panel1.add(labelId);
+		
+		spinnerID = new JSpinner();
+		panel1.add(spinnerID);
+		
+		panel1.add(panel2);
+		
+		JPanel panelButtons = new JPanel();
+		panelButtons.setLayout(new BoxLayout(panelButtons, BoxLayout.Y_AXIS));
+		
+		edit_user = new JButton("Edit User by ID");
+		edit_user.addActionListener(null);
+		panel2.add(edit_user);
+		
+		// JButton checkout
 		
 		setVisible(true);
 	}
