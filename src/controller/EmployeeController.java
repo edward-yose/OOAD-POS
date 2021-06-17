5fb53e3db96ad0f7d96ea58be5711c438ada17ac
edward-yose <youngedward472@gmail.com>
2021-06-17 18:16:32 +0700

    add status logic di EmployeeController

--- src/controller/EmployeeController.java
+++ src/controller/EmployeeController.java
@@ -4,7 +4,7 @@ import model.Employee;
 
 public class EmployeeController {
 	
-	public static int logEmployeeIn (String username, String password) {
+	public static int logEmployeeIn (String username, String password, String status) {
 		// TO-DO Login validation
 		
 		Employee user = new Employee();
@@ -13,7 +13,7 @@ public class EmployeeController {
 //		System.out.println("Name is: " + user.getName());
 //		System.out.println("Password is: " + user.getPassword() + ". Entered password is: " + password);
 		
-		if(username.length() > 0 && password.length() > 0) {
+		if(username.length() > 0 && password.length() > 0 &&  status == "active") {
 			if(user.getPassword() != null && user.getPassword().equals(password)) {
 				return user.getRoleID();
 			} else {
