8004aa7f43822feb7a8d4fd3fa0b0edaf87aca13
ClarenceKiva <Clarenceaureliopribadi@gmail.com>
2021-06-17 20:37:23 +0700

    idk what happened

--- src/controller/ProductM_Controller.java
+++ src/controller/ProductM_Controller.java
@@ -13,8 +13,7 @@ public class ProductM_Controller {
 	}
 	
 	public static int addProduct(Integer id, String name, String description, Integer price, Integer stock) {
-		 Product prod = new Product();
-		 prod = Product.AddProduct(product);
+		
 		
 		
 		
