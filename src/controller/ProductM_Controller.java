c217b609126c28a69ffd0e9095cf23dd74895439
ClarenceKiva <Clarenceaureliopribadi@gmail.com>
2021-06-17 14:27:18 +0700

    Update ProductM_Controller.java

--- src/controller/ProductM_Controller.java
+++ src/controller/ProductM_Controller.java
@@ -1,5 +1,22 @@
 package controller;
 
+import java.util.List;
+
+import view.View_Product;
+import model.Product;
+
 public class ProductM_Controller {
 
+	public static List<Product> getAllProduct(){ 
+		return Product.getAllProducts();
+	}
+	
+	public static String addProduct(int id, String name, String description, int price, int stock) {
+		
+		
+		
+		
+		return description;
+		
+	}
 }
